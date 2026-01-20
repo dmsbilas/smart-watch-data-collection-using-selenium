81193157c18093d5da7cf7692cbe7122c125774c
Abu@mac19 <dmsbilas@yahoo.com>
2026-01-20 19:54:50 +0600

    found all products

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import get_daraz_smart_watches
 
 daraz_website_url_with_search_query = "https://www.daraz.com.bd/catalog/?q=smart%20watch"
-
+page_current_count = 25
 daraz_watch_finder  = get_daraz_smart_watches.DarazSmartWatchFinder(daraz_website_url_with_search_query)
+total_search_result_pages = daraz_watch_finder.GetPageCount()
+#daraz_watch_finder.GetPageCount()
+while   page_current_count <= total_search_result_pages:
+    daraz_watch_finder  = get_daraz_smart_watches.DarazSmartWatchFinder(daraz_website_url_with_search_query+"&page="+str(page_current_count) +"")
+    divs = daraz_watch_finder.get_product_divs()
+    daraz_watch_finder.write_product_urls_to_file(divs)
+    print("Completed page: ", page_current_count, " of ", total_search_result_pages)
+    page_current_count += 1
 
 
-divs = daraz_watch_finder.get_product_divs()
-daraz_watch_finder.write_product_urls_to_file(divs)
+
+
 
 
--- get_daraz_smart_watches.py
+++ get_daraz_smart_watches.py
@@ -8,21 +8,28 @@ class DarazSmartWatchFinder:
     
     driver = None
     _url = None
+    current_page = 1
+    total_pages = 1
 
     def __init__(self, url):
         #check if url is valid structure
         if not self.validators.url(url):
             raise ValueError("Invalid URL structure")
-        
         self.driver = self.initialize_driver(url)
         self._url = url
+        self.current_page = 1
+        if self.GetSearchResultPageCount() > 0:
+            self.total_pages = self.GetSearchResultPageCount()
+
+    def GetPageCount(self):
+        return self.total_pages
+
 
     def get_product_divs(self):
         product_item_divs = self.driver.find_elements(self.By.XPATH, '//div[@data-qa-locator="product-item"]')
         return product_item_divs
 
     def write_product_urls_to_file(self, product_item_divs):
-        self.GetPageCount()
         for div in product_item_divs:
             anchor_element = div.find_element(self.By.TAG_NAME, 'a')
             product_url = anchor_element.get_attribute('href')
@@ -30,6 +37,7 @@ class DarazSmartWatchFinder:
             with open('daraz_smart_watches_urls.txt', 'a') as f:
                 f.write(product_url + '\n')
 
+        self.driver.quit()
 
     def initialize_driver(self, url):
         self.driver = self.webdriver.Chrome(service=self.Service(self.ChromeDriverManager().install()))
@@ -37,7 +45,7 @@ class DarazSmartWatchFinder:
         self.time.sleep(5)
         return self.driver
 
-    def GetPageCount(self):
+    def GetSearchResultPageCount(self):
         elements = self.driver.find_elements(
             self.By.CSS_SELECTOR,
             "li.ant-pagination-item a"
@@ -48,5 +56,4 @@ class DarazSmartWatchFinder:
             if el.text.isdigit()
         ]
         max_page = max(pages)
-        print(max_page)
         return max_page
